e357b1272aba5114d06d240d52c5641b7704ee40
DharmThanki1629 <dharmthanki.atmiya@gmail.com>
2026-05-14 23:30:25 +0530

    updated cors for vercel

--- client/src/components/LogHealth.js
+++ client/src/components/LogHealth.js
@@ -101,9 +101,12 @@ function LogHealth() {
                   <label style={styles.label}>{field.icon} {field.label}</label>
                   <input
                     style={{ ...styles.input, borderColor: form[field.name] ? field.color : '#e2e8f0' }}
-                    type="number" name={field.name}
+                    type="number"
+                    name={field.name}
                     placeholder={field.placeholder}
-                    onChange={handleChange} min="0"
+                    onChange={handleChange}
+                    min="0"
+                    step={field.name === 'weight' ? '0.1' : '1'}
                   />
                   <div style={styles.inputDesc}>{field.desc}</div>
                 </div>
