1fe07a722a34c27cb0e586565efd3a20a4eaf2f8
DharmThanki1629 <dharmthanki.atmiya@gmail.com>
2026-05-14 23:12:06 +0530

    updated cors for vercel

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -72,7 +72,7 @@ function Register() {
               <div style={styles.inputWrap}>
                 <span style={styles.inputIcon}>👤</span>
                 <input style={styles.input} name="name"
-                  placeholder="" type="text"
+                  placeholder="Name" type="text"
                   onChange={handleChange} required />
               </div>
             </div>
